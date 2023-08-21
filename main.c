2838ddbd5d239da46979794c77a3117a42940968
Toma Adrian <sirhadrian21@gmail.com>
2023-08-21 18:10:24 +0300

    working texture loader

--- main.c
+++ main.c
@@ -113,7 +113,8 @@ int main() {
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, imgWidth, imgHeigth, 0, GL_RGBA,
+  // For images with an alpha channel use GL_RGBA
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, imgWidth, imgHeigth, 0, GL_RGB,
                GL_UNSIGNED_BYTE, bytes);
 
   glGenerateMipmap(GL_TEXTURE_2D);
