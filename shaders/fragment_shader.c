4c570ea96abe907a8cf2bc5fd828aff1b757d901
Toma Adrian <sirhadrian21@gmail.com>
2023-10-17 13:54:50 +0300

    basic shaders

---
+++ shaders/fragment_shader.c
@@ -0,0 +1,30 @@
+#version 460 core
+layout(location = 0) out vec4 FragColor;
+
+in vec3 color;
+in vec2 texCoord;
+
+uniform sampler2D tex0;
+// Load texture
+// vec4 text = texture(tex0, texCoord * vec2(-1., 1.));
+
+uniform float u_time;
+uniform vec2 u_resolution;
+uniform vec2 u_mouse;
+
+#define T u_time
+#define R u_resolution
+#define FC gl_FragCoord
+#define M u_mouse
+
+#define PI 3.14159265359
+#define S(a, b, x) smoothstep(a, b, x)
+
+void main() 
+{
+  vec2 uv = FC.xy / R.xy;
+
+  vec3 col = vec3(0.);
+
+  FragColor = vec4(col, 1.);
+}
