a9988d468fc6f3b866d4d8295c50978136ff0334
Toma Adrian <sirhadrian21@gmail.com>
2023-10-20 12:46:42 +0300

    format

--- logger.c
+++ logger.c
@@ -3,7 +3,9 @@
 #include <stdlib.h>
 #include <time.h>
 
-void logger(const char* tag, const char* message) {
+void 
+logger(const char* tag, const char* message) 
+{
    time_t now;
    time(&now);
    printf("%s [%s]: %s\n", ctime(&now), tag, message);
